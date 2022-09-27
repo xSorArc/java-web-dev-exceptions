04c8e0f9bc2454d9663507f8063253aa53188c93
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-09-27 14:32:18 -0500

    Finished exercises

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,31 +1,57 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        System.out.println(Divide(10, 0));
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Jeff", "NoJava");
 
         // Test out your CheckFileExtension() function!
+        System.out.println(CheckFileExtension(studentFiles.get("Brad")));
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static int Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You can't divide by 0.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return x / y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        // If a student’s submitted file ends in .java, they get 1 point.
+        // If a student’s submitted file doesn’t end in .java, they get 0 points.
+        // If the file submitted is null or an empty string, an exception should be thrown, and you
+        // should give the student -1 points. What kind of exception is up to you, including to
+        // a custom exception!
+
+        if (fileName == null || fileName.equals("")) {
+            try {
+                throw new NoFileSubmittedException("File must be turned in to get credit!");
+            } catch (NoFileSubmittedException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        }
+
+        if (fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
 }
---
+++ src/org/launchcode/exercises/NoFileSubmittedException.java
@@ -0,0 +1,7 @@
+package org.launchcode.exercises;
+
+public class NoFileSubmittedException extends Exception {
+    public NoFileSubmittedException(String message) {
+        super(message);
+    }
+}
